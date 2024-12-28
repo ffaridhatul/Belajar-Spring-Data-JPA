0392fc4b153e61158d3cd53d1ecdefe66672bed0
ffaridhatul <faridhatulzikri11@gmail.com>
2024-12-28 12:13:11 +0700

    QueryMethod findFirstByName

--- src/main/java/enigma_camp/spring_data_jpa/repositori/CategoryRepository.java
+++ src/main/java/enigma_camp/spring_data_jpa/repositori/CategoryRepository.java
@@ -3,5 +3,14 @@ package enigma_camp.spring_data_jpa.repositori;
 import enigma_camp.spring_data_jpa.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    //Where name = ?
+    Optional<Category> findFirstByName(String name);
+
+    //Where name like ?
+    List<Category> findALlByNameLike(String name);
 }
--- src/test/java/enigma_camp/spring_data_jpa/service/CategoryServiceTest.java
+++ src/test/java/enigma_camp/spring_data_jpa/service/CategoryServiceTest.java
@@ -1,14 +1,21 @@
 package enigma_camp.spring_data_jpa.service;
 
+import enigma_camp.spring_data_jpa.entity.Category;
 import enigma_camp.spring_data_jpa.repositori.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CategoryServiceTest {
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Autowired
     private CategoryService categoryService;
@@ -34,4 +41,15 @@ class CategoryServiceTest {
         });
     }
 
+    @Test
+    void queryTestMethod() {
+        Category category = categoryRepository.findFirstByName("Masakan Padang").orElse(null);
+        assertNotNull(category);
+        assertEquals("Masakan Padang", category.getName());
+
+        List<Category> aLlByNameLike = categoryRepository.findALlByNameLike("%Masakan%");
+        assertEquals(1, aLlByNameLike.size());
+        assertEquals("Masakan Padang", aLlByNameLike.get(0).getName());
+
+    }
 }
